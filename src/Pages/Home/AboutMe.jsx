d7dc339a097adeb47e0e6ce8ef2007f4b5794ed6
g3rm4n <geavak@taltech.ee>
2024-04-29 20:41:59 +0300

    tweaks

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -4,8 +4,8 @@ export default function AboutMe() {
       <div className="about--section--img">
         <img src="./img/german-avakjan.jpg" alt="Hero Section" />
       </div>
-      <div className="hero--section--content--box about--section--box">
-        <div className="hero--section--content">
+      <div className="about--section--content--box about--section--box">
+        <div className="about--section--content">
           <p className="about--section--description">
             I'm studying computer science at TalTech University. My journey in tech started out of curiosity about how software can change things, and now, I'm all about creating meaningful solutions. I enjoy problem-solving and always look for new learning opportunities. These days, I'm really into software development, with a goal to contribute to projects that make a positive impact. Thanks for stopping by!
           </p>
