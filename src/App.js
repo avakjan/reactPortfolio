409be98b63c52f6eb81fb2d57594a97dfdad3ca1
g3rm4n <geavak@taltech.ee>
2024-05-11 21:40:32 +0300

    appearing/disappearing navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
 import Home from "./Pages/Home/Homescreen";
+import useScrollListener from "./Pages/Home/hooks/useScrollListener/index.js";
+import "./App.css"
 
 function App() {
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [navHidden, setNavHidden] = useState(false);
+  const scroll = useScrollListener();
+
+  useEffect(() => {
+    const handleNavVisibility = () => {
+      if (scroll.y > lastScrollY && scroll.y > 100) {
+        // Scrolling down
+        if (!navHidden) setNavHidden(true);
+      } else if (scroll.y < lastScrollY) {
+        // Scrolling up
+        if (navHidden) setNavHidden(false);
+      }
+      setLastScrollY(scroll.y);
+    };
+
+    handleNavVisibility();
+  }, [scroll.y, lastScrollY, navHidden]);
+
   return (
     <div className="App">
       <Router>
-        <div>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="*" element={<div>404 Not Found</div>}></Route>
-          </Routes>
-        </div>
+        <nav className={navHidden ? "nav-bar--hidden" : ""}>
+          <Navbar/>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="*" element={<div>404 Not Found</div>}></Route>
+        </Routes>
       </Router>
     </div>
   );
